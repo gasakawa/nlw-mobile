3ebccad239230ed6b863dbd3162ea1851d728a0a
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-04 23:27:35 -0300

    feat: finish home page

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { AppLoading } from 'expo';
 
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 const App = () => {
   const [fontsLoaded] = useFonts({
@@ -23,7 +23,7 @@ const App = () => {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
 import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  const handleNavigationToPoints = () => {
+    navigation.navigate('Point');
+  };
+
   return (
     <ImageBackground
       style={styles.container}
@@ -16,7 +25,16 @@ const Home = () => {
         </Text>
       </View>
 
-      <View style={styles.footer}></View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
     </ImageBackground>
   );
 };
@@ -25,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: '#f0f0f5',
   },
 
   main: {
